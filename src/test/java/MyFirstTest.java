f30ce51a37e904295cbfb908c82fed28589db4c6
Igor Rudenko <fac30ff@gmail.com>
2018-11-21 17:11:44 +0200

    second commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+  private WebDriver driver;
+  private WebDriverWait driverWait;
+
+  @BeforeEach
+  public void start() {
+    driver = new ChromeDriver();
+    driverWait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.get("http://www.google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("btnG")).click();
+    driverWait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+  @AfterEach
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
